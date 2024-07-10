900059ebd6fb464db685bd3c98c2e9e168663b48
Chakradhar Gorijavolu <164984579+ChakradharGorijavolu@users.noreply.github.com>
2024-07-10 09:23:25 +0530

    Small changes in FCFS

--- Algorithms/Fcfs.cpp
+++ Algorithms/Fcfs.cpp
@@ -1,31 +1,54 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <string>
+
 using namespace std;
 
-class FCFS{
-    public:
-    void sort(int PID){                     //Cpu scheduling algorithm but its having convey effect
-        int WT[PID],BT[PID];
-        for(int i=1;i<=PID;i++){
-            cin>>WT[i];
-        }
-        for(int i=1;i<=PID;i++){
-            cin>>BT[i];
-        }
-        int TAT=0;
-        for(int i=1;i<=PID;i++){
-            TAT=WT[i]+BT[i];
+class FCFS {
+public:
+    void sort(int PID) {                                                    // Cpu scheduling algorithm but it's having convey effect
+        vector<int> WT(PID), BT(PID);
+        vector<string> P(PID);
+
+        cout << "Enter process IDs (strings):" << endl;
+        for (int i = 0; i < PID; i++) {
+            cin >> P[i];
         }
 
-        for(int i=0;i<PID;i++){
-            cout<<TAT<<" ";
+        cout << "Enter waiting times:" << endl;
+        int AWT=0;
+        for (int i = 0; i < PID; i++) {
+           
+            cin >> WT[i];
+            AWT=AWT+WT[i];
         }
+
+        cout << "Enter burst times:" << endl;
+        for (int i = 0; i < PID; i++) {
+            cin >> BT[i];
+        }
+
+        cout << "Turnaround times:" << endl;
+        int ATAT=0;
+        for (int i = 0; i < PID; i++) {
+            int TAT = WT[i] + BT[i];
+            ATAT=ATAT+TAT;
+            cout << "Process " << P[i] << ": " << TAT << endl;
+        }
+
+        cout << "Avg Waiting time: " << AWT/PID << " Units"<<endl;
+        cout << "Avg Turnaround time: " << ATAT/PID << " Units" <<endl;
+        
     }
 };
 
-int main(){
+int main() {
     int PID;
-    cin>>PID;
+    cout << "Enter number of processes: ";
+    cin >> PID;
 
     FCFS obj;
     obj.sort(PID);
+
+    return 0;
 }
